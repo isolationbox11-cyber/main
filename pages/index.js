50d756140bb88a1a7722858a58725b8649e5ea71
isolation box <isolationbox11@gmail.com>
2025-08-03 10:48:50 -0400

    Create Next.js homepage

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>Welcome to IsolationBox</h1>
+      <p>Your Next.js & Vercel project is now deployed successfully!</p>
+    </div>
+  );
+}
