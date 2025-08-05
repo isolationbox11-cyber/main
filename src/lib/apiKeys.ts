7aac80faedde2f3856dc8063f1e1c7f430a66366
isolation box <isolationbox11@gmail.com>
2025-08-04 23:44:57 -0400

    Update API keys in apiKeys.ts

---
+++ src/lib/apiKeys.ts
@@ -0,0 +1,10 @@
+export const API_KEYS = {
+  greynoise: "",
+  shodan: "lmkycaLjbVoQTlA81pCtW6TdfYwa4wjv",
+  alienvault_otx: "e35df69a6f2f18c1b5acd6202632a58cfe90ff2ae4475a20855bcbf6d8f9b884",
+  abuseipdb: [
+    "deabac5bd7e9de2be72767151e6456fdc662960d90266ca15f17bc5c7c08bd068b9f607986b0af22",
+    "47c9836ea7af51fb6ef7e1e406313beb5bdaeaff909ad789"
+  ],
+  virustotal: "de0e621441bce18144a9a2eebd246ecce00c0eda7fcaf3c9612be0598365f2b0"
+};
